7e691d221358fd34ae11fa4343aa9d701c1ce467
Khuong Nguyen <khuongntrd@gmail.com>
2017-07-26 22:25:47 +0700

    Add project files.

---
+++ ChatLa.Client/ChatLa.Client/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Xamarin.Forms;
+
+namespace ChatLa.Client
+{
+    public class Message
+    {
+        public string Content { get; set; }
+        public bool Mine { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        private IHubProxy ChatHub { get; }
+        private HubConnection Connection { get; }
+
+        public ObservableCollection<Message> Collection { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Collection = new ObservableCollection<Message>();
+            Connection = new HubConnection("http://192.168.54.2:5000");
+            ChatHub = Connection.CreateHubProxy("ChatHub");
+            ChatHub.On("Ready", OnReady);
+            ChatHub.On<string, string>("Message", OnMessage);
+
+            MainListView.ItemsSource = Collection;
+        }
+
+        private void OnMessage(string message, string userId)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Message() { Content = message, Mine = userId == Connection.ConnectionId };
+                Collection.Add(msg);
+            });
+        }
+
+        public bool IsReady { get; set; } = false;
+        private void OnReady()
+        {
+            IsReady = true;
+
+            UpdateStatus("Người lạ đã vào phòng, bạn có thể chat");
+        }
+
+        private async void ActionClicked(object sender, EventArgs e)
+        {
+            MainButton.IsEnabled = false;
+
+            await Connection.Start();
+
+            if (IsReady == false)
+                UpdateStatus("Đã vào, đang chờ người lạ");
+        }
+
+
+        private void UpdateStatus(string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                StatusLabel.Text = message;
+            });
+        }
+
+        private async void SendOnClicked(object sender, EventArgs e)
+        {
+            ChatButton.IsEnabled = false;
+            await ChatHub.Invoke("Chat", ChatEntry.Text);
+            ChatEntry.Text = string.Empty;
+            ChatButton.IsEnabled = true;
+        }
+
+        private async void ChatEntry_OnCompleted(object sender, EventArgs e)
+        {
+            ChatButton.IsEnabled = false;
+            await ChatHub.Invoke("Chat", ChatEntry.Text);
+            ChatEntry.Text = string.Empty;
+            ChatButton.IsEnabled = true;
+        }
+    }
+}
---
+++ ChatLa.Server/ChatHub.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatLa.Server
+{
+    public class Room
+    {
+        public string Id { get; set; }
+        public List<string> Users { get; set; }
+
+        public Room()
+        {
+            Id = Guid.NewGuid().ToString();
+            Users = new List<string>();
+        }
+    }
+    public class ChatHub : Hub
+    {
+        public static List<Room> Rooms { get; set; } = new List<Room>();
+        
+        public override async Task OnConnected()
+        {
+            await base.OnConnected();
+
+            var room = Rooms.FirstOrDefault(r => r.Users.Count < 2);
+
+            if (room != null)
+            {
+                room.Users.Add(Context.ConnectionId);
+            }
+            else
+            {
+                room = new Room();
+                room.Users.Add(Context.ConnectionId);
+                Rooms.Add(room);
+            }
+
+            if (room.Users.Count == 2)
+            {
+                Clients.Clients(room.Users).Ready();
+            }
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void Chat(string message)
+        {
+            var room = Rooms.FirstOrDefault(x => x.Users.Contains(Context.ConnectionId));
+            if (room != null)
+            {
+                Clients.Clients(room.Users).Message(message, Context.ConnectionId);
+            }
+        }
+    }
+}
